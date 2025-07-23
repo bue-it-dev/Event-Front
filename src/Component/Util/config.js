ea24bf08a4658faf19a59897bec0b9e886636b46
Omar Alkotb <omaralkotb20@gmail.com>
2025-07-23 10:35:29 +0300

    added the connection string to preproduction database

--- src/Component/Util/config.js
+++ src/Component/Util/config.js
@@ -1,7 +1,7 @@
 const config = {
-  // BASE_URL: "https://hcms.bue.edu.eg/TravelBE",
+  BASE_URL: "https://hcms.bue.edu.eg/EventBE",
   //BASE_URL: "https://localhost:44363",
-  BASE_URL: "https://localhost:7138",
+  // BASE_URL: "https://localhost:7138",
 };
 
 export default config;
